f1dbe2750e22f6b26de26c95067012701b14ae14
radeqq007 <radoslaw.kaczmarczyk@proton.me>
2026-02-26 21:18:29 +0100

    feat: load config

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.6.0
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/colorprofile v0.4.2 // indirect
 	github.com/charmbracelet/ultraviolet v0.0.0-20251205161215-1948445e3318 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,16 @@
 package config
 
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/BurntSushi/toml"
+)
+
+type fileConfig struct {
+	Colors ColorsConfig `toml:"colors"`
+}
+
 type LayoutConfig struct {
 	BorderWidth      int
 	HeaderFooterSize int
@@ -8,25 +19,25 @@ type LayoutConfig struct {
 }
 
 type ColorsConfig struct {
-	PaneBorder         string
-	FocusedPaneBorder  string
-	SelectedFileBG     string
-	SelectedFileFG     string
-	DirFG              string
-	FileFG             string
-	CmdBoxFG           string
-	CmdBoxBorder       string
-	AlertNormalFG      string
-	AlertNormalBorder  string
-	AlertInfoFG        string
-	AlertInfoBorder    string
-	AlertWarningFG     string
-	AlertWarningBorder string
-	AlertErrorFG       string
-	AlertErrorBorder   string
-	ErrorFG            string
-	ErrorBG            string
-	ErrorBorder        string
+	PaneBorder         string `toml:"pane_border"`
+	FocusedPaneBorder  string `toml:"focused_pane_border"`
+	SelectedFileBG     string `toml:"selected_file_bg"`
+	SelectedFileFG     string `toml:"selected_file_fg"`
+	DirFG              string `toml:"dir_bg"`
+	FileFG             string `toml:"file_fg"`
+	CmdBoxFG           string `toml:"cmd_box_fg"`
+	CmdBoxBorder       string `toml:"cmd_box_border"`
+	AlertNormalFG      string `toml:"alert_normal_fg"`
+	AlertNormalBorder  string `toml:"alert_normal_border"`
+	AlertInfoFG        string `toml:"alert_info_fg"`
+	AlertInfoBorder    string `toml:"alert_info_border"`
+	AlertWarningFG     string `toml:"alert_warning_fg"`
+	AlertWarningBorder string `toml:"alert_warning_border"`
+	AlertErrorFG       string `toml:"alert_error_fg"`
+	AlertErrorBorder   string `toml:"alert_error_border"`
+	ErrorFG            string `toml:"error_fg"`
+	ErrorBG            string `toml:"error_bg"`
+	ErrorBorder        string `toml:"error_border"`
 }
 
 var Layout = LayoutConfig{
@@ -53,4 +64,27 @@ var Colors = ColorsConfig{
 	AlertErrorBorder:   "9",
 	AlertWarningFG:     "220",
 	AlertWarningBorder: "220",
+}
+
+func Load() error {
+	configDir, _ := os.UserConfigDir()
+	configPath := filepath.Join(configDir, "duvet", "config.toml")
+	if _, err := os.Stat(configPath); err != nil && os.IsNotExist(err) {
+		return nil
+	}
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return err
+	}
+
+	var fc fileConfig
+	fc.Colors = Colors
+	if err := toml.Unmarshal(data, &fc); err != nil {
+		return err
+	}
+
+	Colors = fc.Colors
+
+	return nil
 }
